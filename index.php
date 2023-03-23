048b053354f57c67778b84a2897da191e60eeb36
MartinaPerozzi <perozzimartina@gmail.com>
2023-03-23 17:48:47 +0100

    in progress

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Creo una classe Movie
-class Products
+class Product
 {
     // Con le sue specifiche caratteristiche
     public $name;
@@ -11,7 +11,7 @@ class Products
 
 
     // Mi passo le variabili con un costruttore e specifico il TIPO di dato per evitare problemi futuri
-    public function __construct(string $_name, string $_description, string $_price, array $_arraycategories)
+    public function __construct(string $_name, string $_description, int $_price, array $_arraycategories)
     {
         $this->name = $_name;
         $this->description = $_description;
@@ -23,9 +23,23 @@ class Products
 class Categorie
 {
     public $name;
+    public $size;
 
-    public function __construct($_name)
+    public function __construct($_name, $_size)
     {
         $this->name = $_name;
+        $this->size = $_size;
     }
 }
+
+$foodS = new Categorie("croquettes", "small");
+$foodM = new Categorie("croquettes", "medium");
+$foodL = new Categorie("croquettes", "large");
+$bedS = new Categorie("bed", "small");
+$bedM = new Categorie("bed", "medium");
+$bedM = new Categorie("bed", "large");
+$toys = new Categorie("ball", "unique");
+
+$products = [
+    new Product("Pet Food", "Complemental food for your pet", 12.50, [$foodS]),
+];
